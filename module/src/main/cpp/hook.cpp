d9f56a55066cf1024b1a8df55e86217af32fac08
Feryabd <138967632+Feryabd@users.noreply.github.com>
2026-07-23 02:04:27 +0700

    Implement unlimited ammo feature and refactor hooks
    Refactor hook.cpp to include new functionality for unlimited ammo and modify memory hooking logic.

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -1,107 +1,39 @@
-#include <cstring>
-#include <cstdio>
 #include <unistd.h>
-#include <sys/system_properties.h>
-#include <dlfcn.h>
-#include <dlfcn.h>
-#include <cstdlib>
-#include <cinttypes>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
-#include "imgui.h"
-#include "imgui_internal.h"
-#include "backends/imgui_impl_opengl3.h"
-#include "backends/imgui_impl_android.h"
-#include "KittyMemory/KittyMemory.h"
-#include "KittyMemory/MemoryPatch.h"
-#include "KittyMemory/KittyScanner.h"
-#include "KittyMemory/KittyUtils.h"
-#include "Includes/Dobby/dobby.h"
-#include "Include/Unity.h"
-#include "Misc.h"
+#include <pthread.h>
 #include "hook.h"
-#include "Include/Roboto-Regular.h"
-#include <iostream>
-#include <chrono>
-#include "Include/Quaternion.h"
-#include "Rect.h"
-#include <fstream>
-#include <limits>
-#define GamePackageName "com.kakaogames.gdts" // define the game package name here please
+#include "dobby.h"
+#include "KittyMemory/KittyMemory.h"
 
-int glHeight, glWidth;
+// Inisialisasi variabel global
+bool g_MenuOpen = true;
+bool feature_UnlimitedAmmo = false;
 
-int isGame(JNIEnv *env, jstring appDataDir)
-{
-    if (!appDataDir)
-        return 0;
-    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
-    int user = 0;
-    static char package_name[256];
-    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
-        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
-            package_name[0] = '\0';
-            LOGW(OBFUSCATE("can't parse %s"), app_data_dir);
-            return 0;
-        }
+// Penampung fungsi amunisi asli game
+int (*orig_get_AmmoInClip)(void *instance) = nullptr;
+
+// Fungsi modifikasi kita untuk membekukan amunisi
+int hook_get_AmmoInClip(void *instance) {
+    if (instance != nullptr && feature_UnlimitedAmmo) {
+        return 9999; // Mengunci amunisi di angka 9999 saat toggle aktif
     }
-    if (strcmp(package_name, GamePackageName) == 0) {
-        LOGI(OBFUSCATE("detect game: %s"), package_name);
-        game_data_dir = new char[strlen(app_data_dir) + 1];
-        strcpy(game_data_dir, app_data_dir);
-        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
-        return 1;
-    } else {
-        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
-        return 0;
-    }
+    return orig_get_AmmoInClip(instance);
 }
 
-bool setupimg;
-
-HOOKAF(void, Input, void *thiz, void *ex_ab, void *ex_ac)
-{
-    origInput(thiz, ex_ab, ex_ac);
-    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz);
-    return;
-}
-
-HOOKAF(int32_t, Consume, void *thiz, void *arg1, bool arg2, long arg3, uint32_t *arg4, AInputEvent **input_event)
-{
-    auto result = origConsume(thiz, arg1, arg2, arg3, arg4, input_event);
-    if(result != 0 || *input_event == nullptr) return result;
-    ImGui_ImplAndroid_HandleInputEvent(*input_event);
-    return result;
-}
-
-#include "functions.h"
-#include "menu.h"
-
-void *hack_thread(void *arg) {
+// Thread yang berjalan di latar belakang mencari libil2cpp.so milik game
+void *hack_thread(void *) {
+    proc_info_t g_il2cppBaseMap;
+    
+    // Looping mencari pendaftaran alamat memori libil2cpp.so
     do {
         sleep(1);
-        g_il2cppBaseMap = KittyMemory::getLibraryBaseMap("libil2cpp.so");
+        g_il2cppBaseMap = KittyMemory::getElfBaseMap("libil2cpp.so");
     } while (!g_il2cppBaseMap.isValid());
-    KITTY_LOGI("il2cpp base: %p", (void*)(g_il2cppBaseMap.startAddress));
-    Pointers();
-    Hooks();
-    auto eglhandle = dlopen("libunity.so", RTLD_LAZY);
-    auto eglSwapBuffers = dlsym(eglhandle, "eglSwapBuffers");
-    DobbyHook((void*)eglSwapBuffers,(void*)hook_eglSwapBuffers,
-              (void**)&old_eglSwapBuffers);
-    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE"));
-    if (NULL != sym_input) {
-        DobbyHook(sym_input,(void*)myInput,(void**)&origInput);
-    } else {
-        sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer7consumeEPNS_26InputEventFactoryInterfaceEblPjPPNS_10InputEventE"));
-        if(NULL != sym_input) {
-            DobbyHook(sym_input,(void *) myConsume,(void **) &origConsume);
-        }
-    }
-    LOGI("Draw Done!");
+
+    // Alamat Offset target amunisi Pixel Gun 3D Anda: 0x58C46DC
+    uintptr_t ammo_address = g_il2cppBaseMap.startAddress + 0x58C46DC;
+
+    // Menjalankan fungsi pengaitan memori via DobbyHook
+    DobbyHook((void *)ammo_address, (void *)hook_get_AmmoInClip, (void **)&orig_get_AmmoInClip);
+
     return nullptr;
 }
